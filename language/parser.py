9c6e73da91bc3add05d0cfb16f4b63cb562d1417
S. Merzlyakov <s.merzlyakov@voskhod.ru>
2020-06-30 15:32:12 +0300

    небольшие доработки

--- language/parser.py
+++ language/parser.py
@@ -5,34 +5,33 @@ import ply.yacc as yacc
 
 
 def p_program(p):
-    '''program : desc clib'''
+    'program : desc clib'
     print('program')
 
 
 def p_desc(p):
-    '''desc : TITLE COLON SPACE line EOL'''
+    'desc : TITLE COLON SPACE string EOL'
     print('title: ' + p[4])
 
 
 def p_clib(p):
-    '''clib : CLIB COLON SPACE WORD EOL'''
+    'clib : CLIB COLON SPACE WORD EOL'
     print('clib: ' + p[4])
 
 
-def p_line_short(p):
-    '''line : WORD'''
+def p_string_short(p):
+    'string : WORD'
     p[0] = p[1]
 
 
-def p_line_long(p):
-    '''line : WORD sline'''
-    p[0] = p[1] + p[2]
+def p_string_long(p):
+    'string : string SPACE WORD'
+    p[0] = p[1] + p[2] + p[3]
 
 
-def p_sline(p):
-    '''sline : SPACE line'''
-    p[0] = p[1] + p[2]
-
+#def p_sline(p):
+#    '''sline : SPACE line'''
+#    p[0] = p[1] + p[2]
 
 def p_error(p):
     print('ERROR: ' + str(p))
