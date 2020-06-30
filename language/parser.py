60f7a4453a16f05407bf82c96087e9ff1da4b9fb
S. Merzlyakov <s.merzlyakov@voskhod.ru>
2020-06-30 16:27:11 +0300

    наработки multiline

--- language/parser.py
+++ language/parser.py
@@ -3,30 +3,54 @@ from lexem import *
 
 import ply.yacc as yacc
 
+space_start_pos=0
+
 
 def p_program(p):
-    'program : desc clib'
+    'program : title clib title title'
     print('program')
 
 
-def p_desc(p):
-    'desc : TITLE COLON SPACE string EOL'
-    print('title: ' + p[4])
-
+def p_title(p):
+    'title : TITLE COLON SPACE multiline EOL'
+    print('title: \n' + p[4])
+    global space_start_pos
+    space_start_pos = 0
 
 def p_clib(p):
     'clib : CLIB COLON SPACE WORD EOL'
     print('clib: ' + p[4])
 
 
-def p_string_short(p):
+def p_string_word(p):
     'string : WORD'
+    global space_start_pos
+    assert isinstance(p, yacc.YaccProduction)
+    if space_start_pos == 0:
+        space_start_pos = p.lexpos(1)
     p[0] = p[1]
 
 
-def p_string_long(p):
+def p_string_singleline(p):
     'string : string SPACE WORD'
     p[0] = p[1] + p[2] + p[3]
+
+
+def p_multiline_short(p):
+    'multiline : string'
+    p[0] = p[1]
+
+
+def p_multiline_long(p):
+    'multiline : multiline EOL SPACE string'
+    global space_start_pos
+    print('len p[3] ' + str(len(p[3])))
+    print('space_start_pos ' + str(space_start_pos))
+    if len(p[3]) < space_start_pos:
+        p_error(p[3])
+        return
+    p[0] = p[1] + p[2] + p[3][space_start_pos:] + p[4]
+
 
 
 #def p_sline(p):
--- language/test.py
+++ language/test.py
@@ -15,7 +15,7 @@ lexer = lex.lex()
 parser = yacc.yacc()
 
 
-with open ("tests/schema1.drk", "r") as file:
+with open ("tests/schema2.drk", "r") as file:
     data=file.readlines()
     parser.parse(''.join(data))
     #print(result)
