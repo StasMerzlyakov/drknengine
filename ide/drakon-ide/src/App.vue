ae741a3879a6349556d56884931b2fe6b0d00a28
S Merzlyakov <st.merzlyakov@yandex.ru>
2021-01-11 16:06:43 +0400

    пробуем с vue

---
+++ ide/drakon-ide/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="DRAKON Engine" src="./assets/drakosha104.png">
+  <DrakonEditor msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import DrakonEditor from './components/DrakonEditor.vue'
+
+export default {
+  name: 'App',
+  components: {
+    DrakonEditor
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
