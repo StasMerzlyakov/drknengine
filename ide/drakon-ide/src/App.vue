2501b9d911ba080aa781d3896318488d945ed1c6
S Merzlyakov <st.merzlyakov@yandex.ru>
2021-01-14 00:34:31 +0400

    наработки по локализации

--- ide/drakon-ide/src/App.vue
+++ ide/drakon-ide/src/App.vue
@@ -1,37 +1,27 @@
 <template>
     <div id="app">
             <b-navbar toggleable="lg" type="dark" variant="info">
-                <b-navbar-brand href="#">NavBar</b-navbar-brand>
+                <b-navbar-brand href="#">{{ $t("messages.editor_name") }}</b-navbar-brand>
             
                 <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
             
                 <b-collapse id="nav-collapse" is-nav>
-                  <b-navbar-nav>
+                  <!-- <b-navbar-nav>
                     <b-nav-item href="#">Link</b-nav-item>
                     <b-nav-item href="#" disabled>Disabled</b-nav-item>
-                  </b-navbar-nav>
+                  </b-navbar-nav> -->
             
                   <b-navbar-nav class="ml-auto">
-                    <b-nav-form>
+                    <!-- <b-nav-form>
                       <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
                       <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-                    </b-nav-form>
+                    </b-nav-form> -->
             
-                    <b-nav-item-dropdown text="Lang" right>
-                      <b-dropdown-item href="#">EN</b-dropdown-item>
-                      <b-dropdown-item href="#">ES</b-dropdown-item>
-                      <b-dropdown-item href="#">RU</b-dropdown-item>
-                      <b-dropdown-item href="#">FA</b-dropdown-item>
-                    </b-nav-item-dropdown>
-            
-                    <b-nav-item-dropdown right>
-                      <template #button-content>
-                        <em>User</em>
-                      </template>
-                      <b-dropdown-item href="#">Profile</b-dropdown-item>
-                      <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-                    </b-nav-item-dropdown>
-                  </b-navbar-nav>
+                     <b-nav-item-dropdown :text="$t('messages.locale')" right>
+                  <!--    <b-dropdown-item href="#" v-for="(lang, index) in langs" :key="index"  v-on:click="$i18n.setLocaleMessage(lang)" :value="lang">{{ lang }}</b-dropdown-item> -->
+                       <b-dropdown-item href="#" v-for="(lang, index) in langs" :key="index"  v-on:click="setLocale(lang)" :value="lang">{{ lang }}</b-dropdown-item>
+                     </b-nav-item-dropdown>
+                   </b-navbar-nav>
                 </b-collapse>
               </b-navbar>
         <!-- <img alt="DRAKON Engine" src="./assets/drakosha104.png"> -->
@@ -45,9 +35,18 @@ import DrakonEditor from './components/DrakonEditor.vue'
 export default {
   name: 'App',
   components: {
-    DrakonEditor
+       DrakonEditor
+    },
+   data () {
+     return { langs: ['ru', 'en'] }
+  },
+  methods: {
+    setLocale (locale) {
+       console.log(locale);
+    }
   }
 }
+  
 </script>
 
 <style>
--- ide/drakon-ide/src/main.js
+++ ide/drakon-ide/src/main.js
@@ -1,13 +1,46 @@
 import Vue from 'vue'
+import VueI18n from 'vue-i18n'
 import App from './App.vue'
 import svgJS from '@/plugins/svg.js'
 import '@/plugins/bootstrap-vue.js'
 
 Vue.config.productionTip = false
 Vue.use(svgJS)
+Vue.use(VueI18n)
+
+const i18n = new VueI18n({
+    locale: 'ru',
+    messages: {
+        'en': {
+            messages: {
+                editor_name: 'DRAKON Editor',
+		locale: 'Lang',
+
+            }
+
+	},
+	'ru': {
+            messages: {
+                editor_name: 'Редактор схем',
+		locale: 'Язык'
+            }
+	}
+    }
+})
+
 
 new Vue({
-  render: h => h(App)
+    i18n,
+    render: h => h(App),
+    data () {
+        return { langs: ['ru', 'en'] }
+    },
+    methods: {
+        setLocale (locale) {
+            console.log(locale);
+            i18n.setLocaleMessage(locale);
+        }
+    }
 }).$mount('#app')
 
 
